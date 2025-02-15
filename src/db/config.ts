1c8076e7227101555b069d96241a9f7b8adffbd1
kumavis <aaron@kumavis.me>
2025-02-14 20:00:58 -1000

    fix: prod db url handling

--- src/db/config.ts
+++ src/db/config.ts
@@ -2,11 +2,7 @@ import { Sequelize } from '@sequelize/core';
 
 export function createSequelize(connectionString: string) {
   const url = new URL(connectionString);
-  const sslMode = url.searchParams.get('sslmode');
-  if (sslMode) {
-    url.searchParams.delete('sslmode');
-    url.searchParams.append('sslmode', sslMode);
-  }
+  url.searchParams.delete('sslmode');
 
   return new Sequelize({
     url: url.toString(),
